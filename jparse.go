74694f09e52cc18d42f792cbdeb60667e0c6fcbe
csharpdf <83098445+csharpdf@users.noreply.github.com>
2021-10-16 23:14:51 -0400

    a

--- jparse.go
+++ jparse.go
@@ -14,24 +14,6 @@ type parse interface {
 func (j *jsonFile) GetValue(name string) interface{} {
 	var decoded map[string]interface{}
 	json.Unmarshal(j.body, &decoded)
-	switch decoded[name].(type) {
-	case string:
-		return decoded[name].(string)
-	case int64:
-		return decoded[name].(int64)
-	case float64:
-		return decoded[name].(float64)
-	case map[string]interface{}:
-		return decoded[name].(map[string]interface{})
-	case []string:
-		return decoded[name].([]string)
-	case []int64:
-		return decoded[name].([]int64)
-	case []float64:
-		return decoded[name].([]float64)
-	case []map[string]interface{}:
-		return decoded[name].([]map[string]interface{})
-	}
 	return decoded[name]
 }
 func (j *jsonFile) SetValue(name string, value string) error {
