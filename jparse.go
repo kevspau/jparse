6b2bf7c07fddea99cc36015680abcb00f14a844a
csharpdf <83098445+csharpdf@users.noreply.github.com>
2021-10-17 00:12:42 -0400

    Update

--- jparse.go
+++ jparse.go
@@ -8,19 +8,14 @@ import (
 type parse interface {
 	GetValue(name string) interface{}
 	SetValue(name string, value string) error
-	Decode() *map[string]interface{}
 }
 
 func (j *jsonFile) GetValue(name string) interface{} {
-	var decoded map[string]interface{}
-	json.Unmarshal(j.body, &decoded)
-	return decoded[name]
+	return j.decode[name]
 }
 func (j *jsonFile) SetValue(name string, value string) error {
-	var decoded map[string]interface{}
-	json.Unmarshal(j.body, &decoded)
-	decoded[name] = value
-	b, e := json.Marshal(decoded)
+	j.decode[name] = value
+	b, e := json.Marshal(decode)
 	if e != nil {
 		return e
 	}
@@ -30,15 +25,11 @@ func (j *jsonFile) SetValue(name string, value string) error {
 	}
 	return nil
 }
-func (j *jsonFile) Decode() *map[string]interface{} {
-	var decoded map[string]interface{}
-	json.Unmarshal(j.body, &decoded)
-	return &decoded
-}
 
 type jsonFile struct {
 	name string
 	body []byte
+	decode *map[string]interface{}
 	parse
 }
 
@@ -47,6 +38,6 @@ func New(file string) (*jsonFile, error) {
 	if e != nil {
 		return nil, e
 	}
-	json := jsonFile{name: file, body: f}
+	json := jsonFile{name: file, body: f, decoded: json.Unmarshal(f, &map[string]interface{})}
 	return &json, nil
 }
