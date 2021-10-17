c4870ae2b8f5f98ed75219d17bd7a4bcc351bb4b
csharpdf <83098445+csharpdf@users.noreply.github.com>
2021-10-17 00:17:48 -0400

    a

--- jparse.go
+++ jparse.go
@@ -15,7 +15,7 @@ func (j *jsonFile) GetValue(name string) interface{} {
 }
 func (j *jsonFile) SetValue(name string, value string) error {
 	j.decode[name] = value
-	b, e := json.Marshal(decode)
+	b, e := json.Marshal(j.decode)
 	if e != nil {
 		return e
 	}
@@ -29,7 +29,7 @@ func (j *jsonFile) SetValue(name string, value string) error {
 type jsonFile struct {
 	name string
 	body []byte
-	decode *map[string]interface{}
+	decode map[string]interface{}
 	parse
 }
 
@@ -38,6 +38,6 @@ func New(file string) (*jsonFile, error) {
 	if e != nil {
 		return nil, e
 	}
-	json := jsonFile{name: file, body: f, decoded: json.Unmarshal(f, &map[string]interface{})}
+	json := jsonFile{name: file, body: f, decode: json.Unmarshal(f, map[string]interface{})}
 	return &json, nil
 }
